938f7cc9aa38bb094d27a00365a2e36c75efe093
Mejri Rached <r.mejri74100@gmail.com>
2020-11-12 11:34:19 +0100

    mise à jour du .gitignore

--- Game/game/map.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by rached on 11/11/2020.
-//
-
-#include "map.h"
--- Game/game/map.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by rached on 11/11/2020.
-//
-
-#ifndef GAME_MAP_H
-#define GAME_MAP_H
-
-#endif //GAME_MAP_H
