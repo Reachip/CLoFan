d361c331e9e9cb4903cadeae12c092eae3e29fb3
Mejri Rached <r.mejri74100@gmail.com>
2020-11-12 11:31:21 +0100

    ajout de la configuration pour l'IDE CLion

---
+++ cmake-build-debug/CMakeFiles/Game.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rached/Documents/CLoFan/game/player.cpp" "/home/rached/Documents/CLoFan/cmake-build-debug/CMakeFiles/Game.dir/game/player.cpp.o"
+  "/home/rached/Documents/CLoFan/main.cpp" "/home/rached/Documents/CLoFan/cmake-build-debug/CMakeFiles/Game.dir/main.cpp.o"
+  "/home/rached/Documents/CLoFan/screens/cScreen.cpp" "/home/rached/Documents/CLoFan/cmake-build-debug/CMakeFiles/Game.dir/screens/cScreen.cpp.o"
+  "/home/rached/Documents/CLoFan/screens/gameBeginningScreen.cpp" "/home/rached/Documents/CLoFan/cmake-build-debug/CMakeFiles/Game.dir/screens/gameBeginningScreen.cpp.o"
+  "/home/rached/Documents/CLoFan/screens/mainMenuScreen.cpp" "/home/rached/Documents/CLoFan/cmake-build-debug/CMakeFiles/Game.dir/screens/mainMenuScreen.cpp.o"
+  "/home/rached/Documents/CLoFan/ui/button.cpp" "/home/rached/Documents/CLoFan/cmake-build-debug/CMakeFiles/Game.dir/ui/button.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Game.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Game.dir/game/player.cpp.o"
+  "CMakeFiles/Game.dir/main.cpp.o"
+  "CMakeFiles/Game.dir/screens/cScreen.cpp.o"
+  "CMakeFiles/Game.dir/screens/gameBeginningScreen.cpp.o"
+  "CMakeFiles/Game.dir/screens/mainMenuScreen.cpp.o"
+  "CMakeFiles/Game.dir/ui/button.cpp.o"
+  "Game"
+  "Game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
