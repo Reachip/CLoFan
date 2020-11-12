03413fb1f353373c63ebc0a5ccf0d0c1485733a5
Mejri Rached <r.mejri74100@gmail.com>
2020-11-12 17:17:20 +0100

    suprpession des attributs inutiles, ajout en public de l'attribut currentPosition

--- game/player.h
+++ game/player.h
@@ -10,29 +10,17 @@
 
 class Player {
 private:
-    std::string name;
     sf::Texture texture;
-    sf::Sprite currentPosition;
-    sf::Sprite leftPosition;
-    sf::Sprite rightPosition;
-    sf::Sprite downPosition;
-    sf::Sprite upPosition;
-
+    std::string name;
     void _addSprite(sf::IntRect);
 
 public:
-    Player(std::string name, std::string spitePath);
-
+    Player(std::string name, std::string spritePath);
+    sf::Sprite currentPosition;
     void move_on_left();
-
     void move_on_right();
-
     void move_up();
-
     void move_down();
-
-    void update(sf::RenderWindow &window);
-
 };
 
 #endif //GAME_PLAYER_H
