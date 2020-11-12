60f6983c184feaa31a1ce6574e206e7e6a8a899e
Mejri Rached <r.mejri74100@gmail.com>
2020-11-12 17:16:04 +0100

    suppression de l'appel à la méthode draw dans les méthodes de mouvement

--- game/player.cpp
+++ game/player.cpp
@@ -8,13 +8,13 @@
 
 Player::Player(std::string name, std::string spritePath) {
     this->name = name;
+
     sf::Texture texture;
 
     if (!texture.loadFromFile(spritePath))
         throw;
 
     this->texture = texture;
-    this->move_up();
 }
 
 void Player::_addSprite(sf::IntRect spritePosition) {
@@ -23,25 +23,21 @@ void Player::_addSprite(sf::IntRect spritePosition) {
 }
 
 void Player::move_up() {
-    _addSprite(sf::IntRect(0, 0, 64, 64));
     this->currentPosition.move(0, -STEP);
+    _addSprite(sf::IntRect(0, 0, 64, 64));
 }
 
 void Player::move_down() {
-    _addSprite(sf::IntRect(0, 64 * 2, 64, 64));
     this->currentPosition.move(0, STEP);
+    _addSprite(sf::IntRect(0, 64 * 2, 64, 64));
 }
 
 void Player::move_on_left() {
-    _addSprite(sf::IntRect(0, 64 * 1, 64, 64));
     this->currentPosition.move(-STEP, 0);
+    _addSprite(sf::IntRect(0, 64 * 1, 64, 64));
 }
 
 void Player::move_on_right() {
-    _addSprite(sf::IntRect(0, 64 * 3, 64, 64));
     this->currentPosition.move(STEP, 0);
-}
-
-void Player::update(sf::RenderWindow &window) {
-    window.draw(this->currentPosition);
+    _addSprite(sf::IntRect(0, 64 * 3, 64, 64));
 }
