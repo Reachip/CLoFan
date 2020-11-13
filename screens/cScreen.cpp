80a065c5db38d20b5b8042e1882041c2f6877f3a
Mejri Rached <r.mejri74100@gmail.com>
2020-11-13 10:59:02 +0100

    ajout d'une méthode static qui replace le personnage si il sort de la fenetre

--- screens/cScreen.cpp
+++ screens/cScreen.cpp
@@ -2,3 +2,26 @@
 // Created by rached on 13/11/2020.
 //
 
+#include "cScreen.h"
+#define ECART_DE_PIXEL_COLLISION_FENETRE 25
+
+void cScreen::handleOutOfWindow(sf::RenderWindow &App, Player &player) {
+    sf::Vector2f playerPosition = player.currentPosition.getPosition();
+    auto windowPosition = App.getSize();
+
+    if (playerPosition.x < 0) {
+        player.currentPosition.setPosition(0, playerPosition.y);
+    }
+
+    if (playerPosition.y < 0) {
+        player.currentPosition.setPosition(playerPosition.x, 0);
+    }
+
+    if (playerPosition.y > windowPosition.y-ECART_DE_PIXEL_COLLISION_FENETRE) {
+        player.currentPosition.setPosition(playerPosition.x, (windowPosition.y-ECART_DE_PIXEL_COLLISION_FENETRE));
+    }
+
+    if (playerPosition.x > windowPosition.x-ECART_DE_PIXEL_COLLISION_FENETRE) {
+        player.currentPosition.setPosition(windowPosition.x-ECART_DE_PIXEL_COLLISION_FENETRE, playerPosition.y);
+    }
+}
--- screens/cScreen.h
+++ screens/cScreen.h
@@ -6,10 +6,12 @@
 #define GAME_CSCREEN_H
 
 #include <SFML/Graphics.hpp>
+#include "../game/player.h"
 
 class cScreen {
 public:
     virtual int Run(sf::RenderWindow &App) = 0;
+    static void handleOutOfWindow(sf::RenderWindow &App, Player &player);
 };
 
 
