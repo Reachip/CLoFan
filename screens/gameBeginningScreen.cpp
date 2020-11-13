6466b05101058acb63fb57d5ae480694b4bb2fc2
Mejri Rached <r.mejri74100@gmail.com>
2020-11-13 01:05:55 +0100

    adaptation du screen avec l'animation de la texture

--- screens/gameBeginningScreen.cpp
+++ screens/gameBeginningScreen.cpp
@@ -14,33 +14,42 @@ int gameBeginningScreen::Run(sf::RenderWindow &App) {
     sf::Event event;
     bool Running = true;
 
-    player.move_up();
     player.currentPosition.setPosition(120, 20);
+    player.move_up();
+    player.update();
 
     while (Running) {
+        bool animPlayer = true;
+
         while (App.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 Running = false;
-
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-                return 0;
-
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-                player.move_up();
-
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-                player.move_down();
-
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-                player.move_on_left();
-
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-                player.move_on_right();
-
-            App.clear();
-            App.draw(player.currentPosition);
-            App.display();
         }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+            return 0;
+
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            player.move_up();
+
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            player.move_down();
+
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            player.move_on_left();
+
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            player.move_on_right();
+
+        else
+            animPlayer = false;
+
+        if (animPlayer)
+            player.update();
+
+        App.draw(player.currentPosition);
+        App.display();
+        App.clear();
     }
 
     return (-1);
