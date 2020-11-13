e4d1aab89ea93c59d0506cee5d9c0a1028087399
jo15122002 <lapinette2002@ymail.com>
2020-11-13 11:49:37 +0100

    Réglage collisions

--- screens/cScreen.cpp
+++ screens/cScreen.cpp
@@ -2,26 +2,34 @@
 // Created by rached on 13/11/2020.
 //
 
+#define HEIGHT 600
+#define WIDTH 1058
+
 #include "cScreen.h"
 #define ECART_DE_PIXEL_COLLISION_FENETRE 25
 
-void cScreen::handleOutOfWindow(sf::RenderWindow &App, Player &player) {
+void cScreen::handleOutOfWindow(sf::RenderWindow &App, Player &player)
+{
     sf::Vector2f playerPosition = player.currentPosition.getPosition();
-    auto windowPosition = App.getSize();
+    sf::Vector2i windowPosition = sf::Vector2i(WIDTH,HEIGHT);
 
-    if (playerPosition.x < 0) {
+    if (playerPosition.x < 0)
+    {
         player.currentPosition.setPosition(0, playerPosition.y);
     }
 
-    if (playerPosition.y < 0) {
+    if (playerPosition.y < 0)
+    {
         player.currentPosition.setPosition(playerPosition.x, 0);
     }
 
-    if (playerPosition.y > windowPosition.y-ECART_DE_PIXEL_COLLISION_FENETRE) {
+    if (playerPosition.y > windowPosition.y-ECART_DE_PIXEL_COLLISION_FENETRE)
+    {
         player.currentPosition.setPosition(playerPosition.x, (windowPosition.y-ECART_DE_PIXEL_COLLISION_FENETRE));
     }
 
-    if (playerPosition.x > windowPosition.x-ECART_DE_PIXEL_COLLISION_FENETRE) {
+    if (playerPosition.x > windowPosition.x-ECART_DE_PIXEL_COLLISION_FENETRE)
+    {
         player.currentPosition.setPosition(windowPosition.x-ECART_DE_PIXEL_COLLISION_FENETRE, playerPosition.y);
     }
 }
