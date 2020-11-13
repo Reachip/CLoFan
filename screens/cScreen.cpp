5f5d83922d6e48878e9750fd0be53f9ff9fc2bc4
Mejri Rached <r.mejri74100@gmail.com>
2020-11-13 16:15:02 +0100

    formatage du code

--- screens/cScreen.cpp
+++ screens/cScreen.cpp
@@ -8,28 +8,23 @@
 #include "cScreen.h"
 #define ECART_DE_PIXEL_COLLISION_FENETRE 25
 
-void cScreen::handleOutOfWindow(sf::RenderWindow &App, Player &player)
-{
+void cScreen::handleOutOfWindow(sf::RenderWindow &App, Player &player) {
     sf::Vector2f playerPosition = player.currentPosition.getPosition();
-    sf::Vector2i windowPosition = sf::Vector2i(WIDTH,HEIGHT);
+    sf::Vector2i windowPosition = sf::Vector2i(WIDTH, HEIGHT);
 
-    if (playerPosition.x < 0)
-    {
+    if (playerPosition.x < 0) {
         player.currentPosition.setPosition(0, playerPosition.y);
     }
 
-    if (playerPosition.y < 0)
-    {
+    if (playerPosition.y < 0) {
         player.currentPosition.setPosition(playerPosition.x, 0);
     }
 
-    if (playerPosition.y > windowPosition.y-ECART_DE_PIXEL_COLLISION_FENETRE)
-    {
-        player.currentPosition.setPosition(playerPosition.x, (windowPosition.y-ECART_DE_PIXEL_COLLISION_FENETRE));
+    if (playerPosition.y > windowPosition.y - ECART_DE_PIXEL_COLLISION_FENETRE) {
+        player.currentPosition.setPosition(playerPosition.x, (windowPosition.y - ECART_DE_PIXEL_COLLISION_FENETRE));
     }
 
-    if (playerPosition.x > windowPosition.x-ECART_DE_PIXEL_COLLISION_FENETRE)
-    {
-        player.currentPosition.setPosition(windowPosition.x-ECART_DE_PIXEL_COLLISION_FENETRE, playerPosition.y);
+    if (playerPosition.x > windowPosition.x - ECART_DE_PIXEL_COLLISION_FENETRE) {
+        player.currentPosition.setPosition(windowPosition.x - ECART_DE_PIXEL_COLLISION_FENETRE, playerPosition.y);
     }
 }
