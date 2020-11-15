80b8a75192c7f4ba46f72789103bdb766be8e4ec
Mejri Rached <r.mejri74100@gmail.com>
2020-11-15 22:11:04 +0100

    impl de la classe messageBox

--- ui/messageBox.cpp
+++ ui/messageBox.cpp
@@ -2,4 +2,48 @@
 // Created by rached on 15/11/2020.
 //
 
+#define FONT_PATH ""
+#define TAILLE 20
+#define POSITION_X 300
+#define POSITION_Y -30
+#define FONT_PATH "./assets/LiberationSerif-Regular.ttf"
+#define ANIMATION_SPEED 2
+
+#include <iostream>
 #include "messageBox.h"
+
+
+messageBox::messageBox(std::string text) {
+    message.setFillColor(sf::Color::White);
+    message.setCharacterSize(TAILLE);
+
+    if (!font.loadFromFile(FONT_PATH)) {
+        throw;
+    }
+
+    message.setFont(font);
+    message.setPosition(POSITION_X, POSITION_Y);
+    message.setString(text);
+}
+
+void messageBox::update(std::string newText) {
+    message.setPosition(POSITION_X, POSITION_Y);
+    message.setString(newText);
+}
+
+void messageBox::draw(sf::RenderTarget &target, sf::RenderStates states) const {
+    states.transform *= getTransform();
+    target.draw(message, states);
+}
+
+void messageBox::animate() {
+    float new_x_position =  message.getPosition().y + ANIMATION_SPEED;
+    message.setPosition(POSITION_X, new_x_position);
+}
+
+bool messageBox::animationIsFinish() {
+    if (message.getPosition().y >= 2)
+        return true;
+
+    return false;
+}
--- ui/messageBox.h
+++ ui/messageBox.h
@@ -5,12 +5,21 @@
 #ifndef GAME_MESSAGEBOX_H
 #define GAME_MESSAGEBOX_H
 
+#include <SFML/Graphics.hpp>
 
+class messageBox : public sf::Drawable, sf::Transformable {
+public:
+    messageBox(std::string text);
+    void animate();
+    virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+    bool animationIsFinish();
+    void update(std::string newText);
 
-class messageBox {
-
+private:
+    sf::Text message;
+    sf::Font font;
+    sf::Clock clock;
 };
-
 
 
 #endif //GAME_MESSAGEBOX_H
