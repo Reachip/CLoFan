131a18e907c68931d4f3c3a96044772d689ef063
jo15122002 <lapinette2002@ymail.com>
2020-11-16 23:58:17 +0100

    Tests de messagebox et entities

--- src/screens/scene5Screen.cpp
+++ src/screens/scene5Screen.cpp
@@ -2,6 +2,10 @@
 #include <SFML/Graphics.hpp>
 #include "scene5Screen.h"
 #include "../game/background.h"
+#include "../game/entities.h"
+
+#include "../ui/messageBox.h"
+#include "../game/bed.h"
 
 #define FULLSTEP 6
 
@@ -17,6 +21,11 @@ scene5Screen::scene5Screen(Player &player) : cScreen(player) {}
 
 int scene5Screen::Run(sf::RenderWindow &App)
 {
+    int nbTouch=0;
+
+    bed lit(300, 360);
+    messageBox message(" ");
+
     bool collision = false;
     sf::Event event;
 
@@ -31,6 +40,11 @@ int scene5Screen::Run(sf::RenderWindow &App)
 
     while (is_running)
     {
+        if(!message.animationIsFinish())
+        {
+            message.animate();
+        }
+
         bool animPlayer = true;
 
         while (App.pollEvent(event))
@@ -81,8 +95,23 @@ int scene5Screen::Run(sf::RenderWindow &App)
             player.update();
 
 
+            if(lit.is_touched(player.currentPosition)&&nbTouch<1)
+            {
+                message.update("ZZZZzzzzz");
+                nbTouch++;
+            }
+            if(!(lit.is_touched(player.currentPosition))){
+                message.clear();
+                nbTouch = 0;
+            }
+
+
+
+
         App.clear();
         App.draw(background);
+        App.draw(message);
+        App.draw(lit);
         App.draw(player);
         App.display();
     }
