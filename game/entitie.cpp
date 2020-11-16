3903cecf1dbcb1993d91c88b24f857914da29d49
Mejri Rached <r.mejri74100@gmail.com>
2020-11-16 09:05:24 +0100

    ajout de la méthode is_touched

--- game/entitie.cpp
+++ game/entitie.cpp
@@ -4,16 +4,21 @@
 
 #include "entitie.h"
 
-entitie::entitie(std::string spritePath) {
+entitie::entitie(std::string spritePath, int x, int y) {
     if (!texture.loadFromFile(spritePath))
         throw;
 
     texture = texture;
     sprite.setTexture(texture);
+    sprite.setPosition(x, y);
 }
 
 void entitie::draw(sf::RenderTarget &target, sf::RenderStates states) const {
     states.transform *= getTransform();
     states.texture = &texture;
     target.draw(sprite, states);
+}
+
+bool entitie::is_touched(sf::Sprite obj) {
+    return sprite.getGlobalBounds().intersects(obj.getGlobalBounds());
 }
--- game/entitie.h
+++ game/entitie.h
@@ -6,14 +6,38 @@
 #define GAME_ENTITIE_H
 
 #include <SFML/Graphics.hpp>
-
+/*
+ * entitie est la classe mère de toutes les entitées du jeu (porte, joueur, fond, PNJ etc.)
+ * Elle hérite des comportements des classes sf::Drawable et sf::Transformable.
+ *
+ * https://www.sfml-dev.org/documentation/2.5.1/classsf_1_1Transformable.php
+ * https://www.sfml-dev.org/documentation/2.0-fr/classsf_1_1Drawable.php
+ *
+ * Dans son constructeur, on demande le chemin vers le sprite et la position initiale de l'entitée.
+ */
 class entitie : public sf::Drawable, public sf::Transformable {
 public:
-    entitie(std::string spritePath);
+    entitie(std::string spritePath, int x, int y);
+
+    /*
+     * Permet d'appliquer la méthode draw sur l'objet.
+     */
     virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const;
 
+    /*
+     * Renvoie true si un sprite touche l'objet, sinon, false.
+     */
+    bool is_touched(sf::Sprite obj);
+
 protected:
+    /*
+     * Texture de l'entitée.
+     */
     sf::Texture texture;
+
+    /*
+     * Sprite de l'entitée.
+     */
     sf::Sprite sprite;
 
 private:
