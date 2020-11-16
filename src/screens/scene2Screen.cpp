7d07f603770560ba70d8606b9804d5df02dea45c
Mejri Rached <r.mejri74100@gmail.com>
2020-11-16 22:18:29 +0100

    ajout de la scene2

--- src/screens/scene2Screen.cpp
+++ src/screens/scene2Screen.cpp
@@ -3,3 +3,57 @@
 //
 
 #include "scene2Screen.h"
+#include "../game/entities.h"
+
+#define SCENE2_MAP "./"
+#define SCENE2_DETAILS "./"
+
+scene2Screen::scene2Screen(Player &player) : cScreen(player) {}
+
+int scene2Screen::Run(sf::RenderWindow &App) {
+    sf::Image details;
+    bool animPlayer = true;
+
+     if (!details.loadFromFile(SCENE2_DETAILS))
+        throw;
+
+    background background(SCENE2_MAP, 0, 0);
+    sf::Event event;
+
+    player.move_up();
+    player.update();
+
+    while (is_running) {
+        while (App.pollEvent(event)) {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape) || event.type == sf::Event::Closed)
+                is_running = false;
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+            return 0;
+
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            handleUp(details);
+
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            handleDown(details);
+
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            handleLeft(details);
+
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            handleRight(details);
+
+        else
+            animPlayer = false;
+
+        if (animPlayer)
+            player.update();
+
+        App.clear();
+        App.draw(background);
+        App.display();
+    }
+
+    return 0;
+}
