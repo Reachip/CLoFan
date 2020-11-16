372a06f7670292d167da25854b4a8c64a51bf4e6
Mejri Rached <r.mejri74100@gmail.com>
2020-11-16 09:52:29 +0100

    documentation de la classe messageBox

--- ui/messageBox.h
+++ ui/messageBox.h
@@ -7,13 +7,34 @@
 
 #include <SFML/Graphics.hpp>
 
+/*
+ * Messagebox est l'objet qui permet d'afficher du texte à l'écran de façon animée.
+ * L'objet peut être mis ç jour afin de changer ou vider son contenu.
+ */
 class messageBox : public sf::Drawable, sf::Transformable {
 public:
     messageBox(std::string text);
+    /*
+     * Méthode qui déroule progressivement le texte.
+     * Elle est conventionnellement appelée quand animationIsFinish
+     * vaut true.
+     */
     void animate();
     virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+
+    /*
+     * Vérifie si l'animation de déroulement du texte est terminée.
+     */
     bool animationIsFinish();
+
+    /*
+     * Change le texte de la messageBox et la remet à son état initial.
+     */
     void update(std::string newText);
+
+    /*
+     * Vide le contenu de la messageBox.
+     */
     void clear();
 
 private:
